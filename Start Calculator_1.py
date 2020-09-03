82cb4176884d4b3980b45399951bd48835dd8db2
KYLE <kyle123c@hotmail.com>
2020-09-03 10:53:54 -0400

    Update Start Calculator_1.py

--- Start Calculator_1.py
+++ Start Calculator_1.py
@@ -1,6 +1,22 @@
 
 ## Define Variables
 
+#Calculates the amount paid towards the principal of the loan, this is the formula paying the principal on the loan after interest bruh bruh.
+def principal_payment(price, down_payment, interest, loan_term, x):
+    r = interest / 1200.0
+    N = loan_term * 12.0
+    P = price - down_payment
+    payment = np.ppmt(r, x, N, P)
+    return payment * (-1)
+
+#Calculates the current monthly expenses   
+def expenses(price, taxes, insurance, maintenance, management, growth, vacancy, rent, x):
+
+
+
+#Calculates the .....
+
+
 Monthly_Rent = input("Enter Monthly Rent\n")
 
 
@@ -8,7 +24,10 @@ Monthly_Rent = input("Enter Monthly Rent\n")
 
 ## Output Code
 
+
+
 Display = """
+principal_payment: {}
 Income:	"""+ Monthly_Rent + """	
 Mortgage Pay:	{}
 Property Tax:	{}
@@ -18,5 +37,7 @@ Other Cost:	{}
 Cash Flow:	{}	
 """
 
+print('******************************************************************************************')
+print('Welcome to Rental Property Calculator')
 print(Display)
 input()
