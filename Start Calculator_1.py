716f8a356c08d887aeafedbd18a5412a69f7784f
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2020-09-06 09:26:27 -0400

    Revert "Update Start Calculator_1.py"
    This reverts commit 82cb4176884d4b3980b45399951bd48835dd8db2.

--- Start Calculator_1.py
+++ Start Calculator_1.py
@@ -1,22 +1,6 @@
 
 ## Define Variables
 
-#Calculates the amount paid towards the principal of the loan, this is the formula paying the principal on the loan after interest bruh bruh.
-def principal_payment(price, down_payment, interest, loan_term, x):
-    r = interest / 1200.0
-    N = loan_term * 12.0
-    P = price - down_payment
-    payment = np.ppmt(r, x, N, P)
-    return payment * (-1)
-
-#Calculates the current monthly expenses   
-def expenses(price, taxes, insurance, maintenance, management, growth, vacancy, rent, x):
-
-
-
-#Calculates the .....
-
-
 Monthly_Rent = input("Enter Monthly Rent\n")
 
 
@@ -24,10 +8,7 @@ Monthly_Rent = input("Enter Monthly Rent\n")
 
 ## Output Code
 
-
-
 Display = """
-principal_payment: {}
 Income:	"""+ Monthly_Rent + """	
 Mortgage Pay:	{}
 Property Tax:	{}
@@ -37,7 +18,5 @@ Other Cost:	{}
 Cash Flow:	{}	
 """
 
-print('******************************************************************************************')
-print('Welcome to Rental Property Calculator')
 print(Display)
 input()
