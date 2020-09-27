4dddde60a81f8e5a8e5a3781eb43469ea5628b36
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2020-09-27 08:00:37 -0400

    Project Archived & Complete
    Created Zipped exe self contained version, final file left in root directory, and moved project files into source folder

--- Menu_GUI.py
+++ Menu_GUI.py
@@ -81,7 +81,7 @@ class MyFrame1 ( wx.Frame ):
 class MyFrame2 ( wx.Frame ):
 	
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.Point( 500,-1 ), size = wx.Size( 500,330 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.Point( -1,-1 ), size = wx.Size( 500,330 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 		
 		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
 		
@@ -93,9 +93,6 @@ class MyFrame2 ( wx.Frame ):
 		self.m_textCtrl5.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INFOTEXT ) )
 		
 		bSizer1.Add( self.m_textCtrl5, 0, wx.ALL|wx.EXPAND, 5 )
-		
-		self.m_button2 = wx.Button( self, wx.ID_ANY, u"New Calculation", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer1.Add( self.m_button2, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 		
 		
 		self.SetSizer( bSizer1 )
